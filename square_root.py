d59ae7861a89b6cb7542d1d064d18c50f4304640
Aero <aerocyberdev@proton.me>
2025-01-19 20:20:28 +0530

    square_root

--- square_root.py
+++ square_root.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
 
-print("Square root is: ", (int(input("Enter number:\t")))**(1/2))
+def sqrt(n):
+    z = 1
+    err = abs(n - (z * z))
+    while err != 0 or err > 1.0e-20:
+        z = (z + (n/z))/2
+        err = abs(n - (z * z))
+
+    return z
+
+print("Square root is: ", sqrt(int(input("Enter number:\t"))))
