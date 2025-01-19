359ccc6fd14eb6a8524fb3d406c056d0107d097e
Aero <aerocyberdev@proton.me>
2025-01-19 21:05:29 +0530

    binary_decimal

---
+++ binary_decimal.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+binary = input("Enter binary:\t")[::-1]
+decimal = 0
+
+for i in range(len(binary)):
+    decimal += int(binary[i]) * (2 ** i)
+
+print("Decimal:\t", str(decimal))
